84394073e6a95898424185a64128afef79d7b647
gurika666 <gurika666@gmail.com>
2023-08-10 14:49:19 +0400

    ⚡️

--- src/js/app.js
+++ src/js/app.js
@@ -1,88 +1,64 @@
-/* Demo JS */
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
-import { gsap } from 'gsap';
-import { DoubleSide, EquirectangularRefractionMapping } from 'three';
-import { AnimationUtils } from 'three';
-
-const hdrTextureUrl = new URL('../../public/assets/bg.hdr', import.meta.url);
-const canvas = document.querySelector('.canvas');
-
-const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xfefefe);
-
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
-const orbit = new OrbitControls(camera, renderer.domElement);
-
-// Camera positioning
-camera.position.set(0, 0, 6);
-orbit.autoRotate = true;
-
-//load manager
-
-const loadManager = new THREE.LoadingManager();
-const loaderDiv = document.querySelector('.loader')
-
-loadManager.onLoad = () =>{
-  setTimeout(() => {
-    
-    loaderDiv.classList.remove('loading');
-  }, 3000);
-}
-
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 0.9;
-const loader = new RGBELoader(loadManager);
-
-loader.load(hdrTextureUrl, function (texture) {
-  texture.mapping = EquirectangularRefractionMapping;
-  scene.background = texture;
-  // scene.environment = texture;
-
-  const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 50, 50),
-    new THREE.MeshStandardMaterial({
-      roughness: 0,
-      metalness: 0,
-      color: 0xffffff,
-      envMap: texture,
-    })
-  );
-
-  scene.add(sphere);
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+let scene, camera, renderer, controls, dynamicTexture, glb,mesh, inputElement;
+let manager = new THREE.LoadingManager;
+let sceneloader = new GLTFLoader(manager);
+manager.onLoad = function (){
+  init();
+  render();
+};
+sceneloader.load("mesh/cube_UV.glb", function(gltf){
+  gltf.scene.traverse((child) => {
+    glb = child
+  })
 });
-
-// Sets a 12 by 12 gird helper
-// const gridHelper = new THREE.GridHelper(12, 12);
-// scene.add(gridHelper);
-
-// Sets the x, y, and z axes with each having a length of 4
-// const axesHelper = new THREE.AxesHelper(4);
-// scene.add(axesHelper);
-
-function animate() {
-  renderer.render(scene, camera);
-  orbit.update();
+function updateCanvas(text) {
+    const canvas = dynamicTexture.image;
+    const context = canvas.getContext("2d");
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.font = "40px Arial";
+    context.fillStyle = "red";
+    context.fillText(text, 10, 50);
+    dynamicTexture.needsUpdate = true;
 }
-
-renderer.setAnimationLoop(animate);
-
-window.addEventListener('resize', resizeEvent);
-
-function resizeEvent() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    controls = new OrbitControls( camera, renderer.domElement);
+    const canvas = document.createElement("canvas");
+    canvas.width = 512;
+    canvas.height = 512;
+    dynamicTexture = new THREE.CanvasTexture(canvas);
+    const material = new THREE.MeshBasicMaterial({ map: dynamicTexture });
+    // const geometry = new THREE.BoxGeometry();
+    // mesh = new THREE.Mesh(geometry, material);
+    glb.material = material;
+    scene.add(glb);
+    inputElement = document.createElement("input");
+    inputElement.type = "text";
+    inputElement.placeholder = "Type something...";
+    document.body.appendChild(inputElement);
+    inputElement.addEventListener("input", function () {
+        const userInput = inputElement.value;
+        updateCanvas(userInput);
+        console.log("input")
+    });
+    updateCanvas("canka!");
 }
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", onWindowResize);
+
